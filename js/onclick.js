935686ee6368185621b50c9fec4da36eadf1df35
shamtime <kylelawsonbak@gmail.com>
2022-10-27 15:19:50 -0700

    fixed audio

--- js/onclick.js
+++ js/onclick.js
@@ -2,7 +2,9 @@ const bobSound = document.getElementById("bobRoss");
 const bobImg = document.getElementById("bobRossImg");
 
 function happyTrees() {
+    bobSound.currentTime = 42;
     bobSound.play();
+    setTimeout(() => bobSound.pause(), 1300);
 };
 
-bobRossImg.addEventListener("click", happyTrees);
+bobImg.addEventListener("click", happyTrees);
