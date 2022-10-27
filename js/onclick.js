6c1a3071635b78e1bd6aeccc958b42b59e36d398
shamtime <kylelawsonbak@gmail.com>
2022-10-27 14:22:15 -0700

    added js script and audio file

---
+++ js/onclick.js
@@ -0,0 +1,8 @@
+const bobSound = document.getElementById("bobRoss");
+const bobImg = document.getElementById("bobRossImg");
+
+function happyTrees() {
+    bobSound.play();
+};
+
+bobRossImg.addEventListener("click", happyTrees());
